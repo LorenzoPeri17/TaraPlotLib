ecb0e42985eb649e32b32579bc335a26aae1fd00
Lorenzo <lp586@cam.ac.uk>
2024-10-31 00:40:23 +0000

    first working draft

---
+++ taraplotlib/__init__.py
@@ -0,0 +1,56 @@
+__all__ = ['add_background'] # type: ignore
+
+from cycler import cycler
+from matplotlib import rcParams
+from matplotlib.font_manager import fontManager
+
+from .src.colors import _add_colors_to_mpl
+from .src.markers import (
+    _add_markers_to_mpl,
+    taralib_paths
+)
+from .src.background import add_background
+
+import warnings
+
+# Markers have been added through a terrible hack
+# that throws a warning. Let's just hide it.
+warnings.filterwarnings("ignore")
+
+_add_colors_to_mpl()
+_add_markers_to_mpl()
+
+rcParams['axes.prop_cycle'] = cycler(
+    color=[
+    '#43978D', 
+    '#F9AD6A',
+    '#264D59',
+    '#813563',
+    ],
+    marker = [
+    'teacup',
+    'teabag',
+    'cat',
+    'pawprint',
+    ])
+
+rcParams["axes.labelpad"] = 1.5
+rcParams["axes.formatter.useoffset"] = False
+rcParams["axes.axisbelow"] = False
+rcParams["axes.unicode_minus"] = False
+
+rcParams["lines.marker"] = 'teacup'
+rcParams["lines.markersize"] = 15
+rcParams["lines.linestyle"] = 'none'
+rcParams["lines.markeredgewidth"] = 1.2
+rcParams["markers.fillstyle"] = 'none'
+
+rcParams["font.family"] = "serif"
+if "Apple Chancery" in [f.name for f in fontManager.ttflist]:
+    rcParams["font.serif"] = ["Apple Chancery"] + rcParams["font.serif"]
+if "PilGi" in [f.name for f in fontManager.ttflist]:
+    rcParams["font.serif"] = ["PilGi"] + rcParams["font.serif"]
+rcParams["font.size"] = 14
+rcParams["axes.labelsize"] = 19
+
+rcParams['figure.autolayout'] = True
---
+++ taraplotlib/src/background.py
@@ -0,0 +1,50 @@
+from matplotlib.transforms import Affine2D
+import matplotlib.patches as patches
+from matplotlib.axes import Axes
+from typing import Literal
+
+from .markers import taralib_paths
+
+def add_background(ax : Axes, 
+                   image : Literal['cat', 'pawprint', 'tecup', 'teabag'], 
+                   loc: tuple[float, float] = (0.975, 0.025), 
+                   scale:float = 0.1,
+                   horizontalalignment : Literal['left', 'center', 'right'] = 'right',
+                   verticalalignment : Literal['top', 'center', 'bottom'] = 'bottom',
+                   linewidth : float = 2,
+                   facecolor : str = 'none',
+                   edgecolor : str = 'TPLBlue',
+                   alpha : float = 0.4,
+                   **kwargs
+                   ) -> None:
+    
+    
+    path = taralib_paths[image]
+    path = path.transformed(Affine2D().scale(scale, scale))
+    # paths come centered
+    # we need to align them properly
+    bbox = path.get_extents()
+    
+    if horizontalalignment == 'left':
+        path = path.transformed(Affine2D().translate(+bbox.width/2, 0))
+    elif horizontalalignment == 'right':
+        path = path.transformed(Affine2D().translate(-bbox.width/2, 0))
+    
+    if verticalalignment == 'top':
+        path = path.transformed(Affine2D().translate(0, -bbox.height/2))
+    elif verticalalignment == 'bottom':
+        path = path.transformed(Affine2D().translate(0, +bbox.height/2))
+    
+    path = path.transformed(Affine2D().translate(*loc))
+    
+    patch = patches.PathPatch(path, 
+                              facecolor=facecolor, 
+                              lw = linewidth,
+                              edgecolor = edgecolor,
+                              alpha = alpha,
+                              **kwargs)
+    
+    patch.set_transform(ax.transAxes)
+    patch.zorder = -1
+    
+    ax.add_patch(patch)
